584df904bc954f947b261484db64b2c77fcfb396
Dennis Bögner <Dennis_boegner@web.de>
2025-11-16 10:43:48 +0100

    change copyright to MIT, add version

--- login.php
+++ login.php
@@ -45,8 +45,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <button type="submit" class="btn-login">Einloggen</button>
         </form>
         <footer>
-            <p>&copy; <?php echo date("Y"); ?> Dennis Bögner</p>
-            <p>Version 0.0.5</p>
+            <p>MIT License - Erstellt von Dennis Bögner</p>
+            <p>Version 1.0.0</p>
         </footer>
     </div>
 </body>
